eaced36336abfa82ec092c8e2fe2317518c87f93
ManI <letz0703@gmail.com>
2022-07-07 15:41:12 +0900

    Initialize firebaseApp

--- src/service/components/login/login.jsx
+++ src/service/components/login/login.jsx
@@ -3,8 +3,11 @@ import Header from "../header/header";
 
 export default function Login({ authService }) {
   const onLogin = (event) => {
-    authService.login(event.currentTarget.textContent);
+    authService //
+      .login(event.currentTarget.textContent)
+      .then(console.log);
   };
+
   return (
     <section>
       <Header />
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,17 +1,11 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
 
-  const firebaseConfig = {
-    apiKey: "procss.env.REACT_APP_FIREBASE_API_KEY"
-    authDomain: "procss.env.REACT_APP_FIREBASE_AUTH_DOMAIN"
-    projectId: "procss.env.REACT_APP_FIREBASE_"
-  };
+const firebaseConfig = {
+  apiKey: procss.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: procss.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: procss.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
