8b29f9b161258382d72eef34c0a4073f069d5a24
ManI <letz0703@gmail.com>
2022-08-06 02:25:12 +0900

    22-0806 2:25 AM

--- src/components/card_edit/card_edit.jsx
+++ src/components/card_edit/card_edit.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-//import styles from './card_edit.module.css'
+import styles from "./card_edit.module.css";
 
 const CardEdit = ({ card }) => {
-  return <h1>{card.id}</h1>;
+  const { name, company, title, email, message, theme, fileName, fileURL } =
+    card;
+  const onSubmit = () => {};
+  return (
+    <form className={styles.form}>
+      <input className={styles.input} type="text" name="name" value={name} />
+      <input
+        className={styles.input}
+        type="text"
+        name="company"
+        value={company}
+      />
+      <select className={styles.select} name="theme" value={theme}>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="colorful">Colorful</option>
+      </select>
+      <input className={styles.input} type="text" name="title" value={title} />
+      <input className={styles.input} type="text" name="email" value={email} />
+      <textarea className={styles.textarea} name="message" value={message} />
+      <div className={styles.fileInput}>{/* <ImageFileInput /> */}</div>
+      {/* <Button name="Delete" onClick={onSubmit} /> */}
+    </form>
+  );
 };
 
 export default CardEdit;
