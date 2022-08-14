87c174d4e5a83eb9340ce65fb7390808275aa70d
ManI <letz0703@gmail.com>
2022-08-15 04:58:31 +0900

    Add Card and Edit component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.module.css";
-import App from "./app";
+import App from "./App";
 import AuthService from "./service/auth_service";
 import UserInfo, { UserContext } from "./common/user_store";
 
