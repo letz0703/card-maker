9f26ab3aa7d1781c236cb0ad3f6fe07f8822376c
ManI <letz0703@gmail.com>
2022-08-31 07:40:27 +0900

    WIP Image upload

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./image_file_input.module.css";
 
-const ImageFileInput = (props) => {
+// const ImageFileInput = (props) => {
+const ImageFileInput = ({ imageUploader }) => {
   return <button>Image</button>;
 };
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import "./index.module.css";
 import App from "./App";
 import AuthService from "./service/auth_service";
 import UserInfo, { UserContext } from "./common/user_store";
+import ImageUploader from "./service/image_uploader";
 
 const authService = new AuthService();
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const imageUploader = new ImageUploader();
+
 root.render(
   <React.StrictMode>
     <App authService={authService} />,
